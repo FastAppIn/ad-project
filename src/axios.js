40582693345b61acf2e7c17df92e1031ca5bab30
Vishvajit Pathak <vp>
2022-02-10 22:42:51 +0530

    Remove unused variables

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,4 @@
 import axios from 'axios';
-const token = '$2b$10$RDewmUZ.M0bvVvGtyArZdOlW2ZapDvh8SAHR7y4A/dZgUrYECnbYy';
-
 const instance = axios.create({
   baseURL: 'http://localhost:3000',
   headers: {
